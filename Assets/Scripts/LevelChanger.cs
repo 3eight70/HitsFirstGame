bf53fda0518c079904c3e800b95c32a6b2aa49aa
Vasilev Danil <111416100+3eight70@users.noreply.github.com>
2023-06-04 19:36:50 +0700

    scene switch + loading screen

--- Assets/Scripts/LevelChanger.cs
+++ Assets/Scripts/LevelChanger.cs
@@ -2,14 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelChanger : MonoBehaviour
 {
     private Animator anim;
     public int levelToLoad;
 
-    public Vector3 position;
-
+    public Slider slider;
+    public GameObject loadingScreen;
 
     private void Start()
     {
@@ -17,13 +18,25 @@ public class LevelChanger : MonoBehaviour
     }
 
     public void FadeToLevel()
-    {
+    {   
         anim.SetTrigger("fade");
     }
 
     public void OnFadeComplete()
     {
-
         SceneManager.LoadScene(levelToLoad);
+        StartCoroutine(LoadingScreenOnFade());
+    }
+
+    IEnumerator LoadingScreenOnFade()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(levelToLoad);
+        loadingScreen.SetActive(true);
+        while(!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+            yield return null;
+        }
     }
 }
