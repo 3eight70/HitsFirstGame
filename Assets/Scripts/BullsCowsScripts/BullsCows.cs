cda1dde256350bd2c126733b71b6651e6b31abea
Volkov Dmitriy <vdy0555@gmail.com>
2023-06-27 11:42:03 +0700

    fixed bulls cows popups, added special class for it

---
+++ Assets/Scripts/BullsCowsScripts/BullsCows.cs
@@ -0,0 +1,102 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BullsCows
+{
+    public class BullsCows : MonoBehaviour
+    {
+        [SerializeField] private InputField UserNumber;
+        [SerializeField] private Transform AnswersContainer;
+        private Algorithm Algorithm;
+
+        private void AddAnswer(string answerText)
+        {
+            Text[] textFields = AnswersContainer.GetComponentsInChildren<Text>();
+
+            var currentAnswerIndex = 2 * Algorithm.AttemptsNum - 1;
+
+            Text textField = textFields[currentAnswerIndex];
+
+            if (textField != null && textField.name != "Index")
+            {
+                textField.text = answerText;
+            }
+        }
+
+        private void OnUserWin(string text)
+        {
+            BullsCowsUI.Instance.ShowWinPopup(text);
+
+            UserNumber.text = "";
+        }
+
+        private void OnTriggerErrorOrLose(string text)
+        {
+            BullsCowsUI.Instance.ShowErrorPopup(text);
+        }
+
+        public void OnUserTryClick()
+        {
+            AlgorithmAnswer result = Algorithm.Execute(UserNumber.text);
+
+            if (result.Status == AlgorithmAnswerStatus.Error)
+            {
+                OnTriggerErrorOrLose(result.ExtraText);
+
+                return;
+            }
+
+            if (result.Status == AlgorithmAnswerStatus.Incorrect)
+            {
+                AddAnswer(result.Text);
+                UserNumber.text = "";
+
+                return;
+            }
+
+            if (result.Status == AlgorithmAnswerStatus.Win)
+            {
+                OnUserWin(result.ExtraText);
+
+                return;
+            }
+
+            if (result.Status == AlgorithmAnswerStatus.Lose)
+            {
+                AddAnswer(result.Text);
+                OnTriggerErrorOrLose(result.ExtraText);
+
+                return;
+            }
+        }
+
+        public void ResetGame()
+        {
+            Text[] textFields = AnswersContainer.GetComponentsInChildren<Text>();
+
+            foreach (var textField in textFields)
+            {
+                if (textField != null && textField.name != "Index")
+                {
+                    textField.text = "";
+                }
+            }
+
+            Algorithm = new Algorithm();
+        }
+
+        public void OnErrorOrLosePopupClose()
+        {
+            if (Algorithm.isLose())
+            {
+                ResetGame();
+                UserNumber.text = "";
+            }
+        }
+
+        void Start()
+        {
+            Algorithm = new Algorithm();
+        }
+    }
+}
--- Assets/Scripts/BullsCowsScripts/BullsCowsUI.cs
+++ Assets/Scripts/BullsCowsScripts/BullsCowsUI.cs
@@ -1,107 +1,29 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace BullsCows
+public class BullsCowsUI : MonoBehaviour
 {
-    public class BullsCowsUI : MonoBehaviour
+    public static BullsCowsUI Instance;
+
+    [SerializeField] private Animator WinAnimator;
+    [SerializeField] private Animator ErrorAnimator;
+    [SerializeField] private Text ErrorText;
+    [SerializeField] private Text WinText;
+
+    void Awake() 
     {
-        public Animator WinAnimator;
-        public Animator ErrorAnimator;
-        public InputField UserNumber;
-        public Text ErrorText;
-        public Text WinText;
-        public Transform AnswersContainer;
-        private Algorithm Algorithm;
+        Instance = this;
+    }
 
-        private void AddAnswer(string answerText)
-        {
-            Text[] textFields = AnswersContainer.GetComponentsInChildren<Text>();
+    public void ShowWinPopup(string text)
+    {
+        WinText.text = text;
+        WinAnimator.SetTrigger("OpenWinPopup");
+    }
 
-            var currentAnswerIndex = 2 * Algorithm.AttemptsNum - 1;
-
-            Text textField = textFields[currentAnswerIndex];
-
-            if (textField != null && textField.name != "Index")
-            {
-                textField.text = answerText;
-            }
-        }
-
-        private void OnUserWin(string text)
-        {
-            WinText.text = text;
-            WinAnimator.SetTrigger("OpenWinPopup");
-
-            UserNumber.text = "";
-        }
-
-        private void OnTriggerErrorOrLose(string text)
-        {
-            ErrorText.text = text;
-            ErrorAnimator.SetTrigger("OnErrorUserNumber");
-        }
-
-        public void OnUserTryClick()
-        {
-            AlgorithmAnswer result = Algorithm.Execute(UserNumber.text);
-
-            if (result.Status == AlgorithmAnswerStatus.Error)
-            {
-                OnTriggerErrorOrLose(result.ExtraText);
-
-                return;
-            }
-
-            if (result.Status == AlgorithmAnswerStatus.Incorrect)
-            {
-                AddAnswer(result.Text);
-                UserNumber.text = "";
-
-                return;
-            }
-
-            if (result.Status == AlgorithmAnswerStatus.Win)
-            {
-                OnUserWin(result.ExtraText);
-
-                return;
-            }
-
-            if (result.Status == AlgorithmAnswerStatus.Lose)
-            {
-                AddAnswer(result.Text);
-                OnTriggerErrorOrLose(result.ExtraText);
-
-                return;
-            }
-        }
-
-        public void ResetGame()
-        {
-            Text[] textFields = AnswersContainer.GetComponentsInChildren<Text>();
-
-            foreach (var textField in textFields)
-            {
-                if (textField != null && textField.name != "Index")
-                {
-                    textField.text = "";
-                }
-            }
-
-            Algorithm = new Algorithm();
-        }
-
-        public void OnErrorOrLosePopupClose()
-        {
-            if (Algorithm.isLose()) {
-                ResetGame();
-                UserNumber.text = "";
-            }
-        }
-
-        void Start()
-        {
-            Algorithm = new Algorithm();
-        }
+    public void ShowErrorPopup(string text)
+    {
+        ErrorText.text = text;
+        ErrorAnimator.SetTrigger("OnErrorUserNumber");
     }
 }
